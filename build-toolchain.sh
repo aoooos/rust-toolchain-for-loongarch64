b26020b30fba88667c4ae569d1d916d5da02b70f
huakui <40330631+jtzhpf@users.noreply.github.com>
2023-07-14 04:15:38 +0800

    Update build-toolchain.sh

--- build-toolchain.sh
+++ build-toolchain.sh
@@ -1,8 +1,8 @@
 set -ex
 
-git clone https://github.com/loongarch-rs/rust.git
+git clone https://github.com/aoooos/rust.git
 cd rust
-git checkout bd32075934ca2825e6b51d246e19670941598240
+#git checkout bd32075934ca2825e6b51d246e19670941598240
 cp ../config.toml ./
 ./x.py build
 ./x.py install
